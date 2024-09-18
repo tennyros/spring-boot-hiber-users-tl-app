43481bb46445b88b64f68e28479dc23e97a0ca35
wendelca <vvkamalov_obs@mail.ru>
2024-09-18 12:53:41 +0500

    init sprign mvn hibernate app commit

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,90 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserService;
+import web.validator.UserValidator;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+    private final UserValidator userValidator;
+    private static final String REDIRECT = "redirect:/";
+
+    @Autowired
+    public UserController(UserService userService, UserValidator userValidator) {
+        this.userService = userService;
+        this.userValidator = userValidator;
+    }
+
+    @GetMapping(value = "/")
+    public String showUsers(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "user/users";
+    }
+
+    @GetMapping(value = "/new")
+    public String newUser(Model model) {
+        model.addAttribute("user", new User());
+        return "user/new_user";
+    }
+
+    @PostMapping(value = "/save")
+    public String saveUser(@RequestParam(value = "firstName") String firstName,
+                           @RequestParam(value = "lastName") String lastName,
+                           @RequestParam(value = "email") String email,
+                           @RequestParam(value = "age", required = false) Integer age,
+                           Model model) {
+        User user = new User(firstName, lastName, email, age);
+        BindingResult result = new BeanPropertyBindingResult(user, "user");
+        userValidator.validate(user, result);
+        if (result.hasErrors()) {
+            model.addAttribute("errors", result.getFieldErrors());
+            model.addAttribute("user", user);
+            return "user/new_user";
+        }
+        userService.addUser(user);
+        return REDIRECT;
+    }
+
+    @GetMapping(value = "/edit")
+    public String editUserForm(@RequestParam(value = "id") Long id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "user/update_user";
+    }
+
+    @PostMapping(value = "/update")
+    public String updateUser(@RequestParam("id") Long id,
+                             @RequestParam("firstName") String firstName,
+                             @RequestParam("lastName") String lastName,
+                             @RequestParam("email") String email,
+                             @RequestParam(value = "age", required = false) Integer age,
+                             Model model) {
+        User user = userService.getUserById(id);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        user.setAge(age);
+        BindingResult result = new BeanPropertyBindingResult(user, "user");
+        userValidator.validate(user, result);
+        if (result.hasErrors()) {
+            model.addAttribute("errors", result.getFieldErrors());
+            model.addAttribute("user", user);
+            return "user/update_user";
+        }
+        userService.updateUser(user);
+        return REDIRECT;
+    }
+
+    @PostMapping(value = "/delete")
+    public String deleteUser(@RequestParam(value = "id") Long id) {
+        userService.deleteUser(id);
+        return REDIRECT;
+    }
+}
---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,85 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false, updatable = false, unique = true)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "age", nullable = false)
+    private Integer age;
+
+    public User() {}
+
+    public User(String firstName, String lastName, String email, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
